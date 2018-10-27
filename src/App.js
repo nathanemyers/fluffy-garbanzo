37e6e7b6086c984e6e6916c9bd2e0c1a289a413e
Nathan Myers <nathanemyers@gmail.com>
2018-10-27 16:04:20 -0500

    adding basic props

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 import SplitHeader from './components/SplitHeader'
 
+const zelda = "http://images.nintendolife.com/news/2017/03/feature_what_happens_when_you_play_breath_of_the_wild_as_a_multiplayer_game/attachment/0/original.jpg"
+const mgs = "https://www.technobuffalo.com/wp-content/uploads/2015/08/Yoji-Shinkawa-Metal-Gear-Solid-art-6.jpg"
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <SplitHeader />
+        <SplitHeader 
+          rightImage={zelda}
+          leftImage={mgs}
+        />
       </div>
     );
   }
--- src/components/SplitHeader.js
+++ src/components/SplitHeader.js
@@ -3,9 +3,44 @@ import React, { Component} from 'react'
 class SplitHeader extends Component {
 
   render() {
+
+    const { rightImage, leftImage } = this.props
+
+    const sharedStyles = {
+      width: "100%",
+      height: "400px",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+    }
+
+    const leftStyle = {
+      backgroundImage: `url(${leftImage})`,
+      ...sharedStyles,
+    }
+
+    const rightStyle = {
+      backgroundImage: `url(${rightImage})`,
+      ...sharedStyles,
+    }
+
     return (
       <div>
         Hello World        
+
+        <div style={leftStyle}></div>
+        <div style={rightStyle}></div>
+
+        <svg className="clip-svg" width="0" height="0">
+          <defs>
+            <clipPath id="left-clip-shape" clipPathUnits="objectBoundingBox" >
+              <polygon points="0 0, 0 0.6, 0.4 1, 1 0" />
+            </clipPath>
+
+            <clipPath id="right-clip-shape" clipPathUnits="objectBoundingBox" >
+              <polygon points="0 1, 0 0, 1 0, 0.7 1" />
+            </clipPath>
+          </defs>
+        </svg>
       </div>
     )
   }
