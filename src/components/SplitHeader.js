fa342563bb1e78a8fc59f1ccc7fe8dc1fe88f027
Nathan Myers <nathanemyers@gmail.com>
2018-10-27 16:38:14 -0500

    cleanup

--- src/components/SplitHeader.js
+++ src/components/SplitHeader.js
@@ -3,35 +3,19 @@ import React, { Component} from 'react'
 class SplitHeader extends Component {
 
   render() {
-
     const { rightImage, leftImage } = this.props
 
     const leftStyle = {
       backgroundImage: `url(${leftImage})`,
     }
-
     const rightStyle = {
       backgroundImage: `url(${rightImage})`,
     }
 
     return (
       <div className="split-header">
-        Hello World        
-
         <div className="split-image left-split" style={leftStyle}></div>
         <div className="split-image right-split" style={rightStyle}></div>
-
-        <svg className="clip-svg" width="0" height="0">
-          <defs>
-            <clipPath id="left-clip-shape" clipPathUnits="objectBoundingBox" >
-              <polygon points="0 0, 0 0.6, 0.4 1, 1 0" />
-            </clipPath>
-
-            <clipPath id="right-clip-shape" clipPathUnits="objectBoundingBox" >
-              <polygon points="0 1, 0 0, 1 0, 0.7 1" />
-            </clipPath>
-          </defs>
-        </svg>
       </div>
     )
   }
