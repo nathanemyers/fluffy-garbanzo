c9d8fefeb6a1301eaa92d602a639a4ffeb6833d5
Nathan Myers <nathanemyers@gmail.com>
2018-10-28 16:30:28 -0500

    hooking up left right state to click

--- src/components/SplitHeader.js
+++ src/components/SplitHeader.js
@@ -1,6 +1,15 @@
 import React, { Component} from 'react'
+import classNames from 'classnames'
 
 class SplitHeader extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      focus: 'center',
+    }
+  }
 
   render() {
     const { rightImage, leftImage } = this.props
@@ -12,10 +21,47 @@ class SplitHeader extends Component {
       backgroundImage: `url(${rightImage})`,
     }
 
+    const leftClickHandler = () => {
+      console.log('left clicked!')
+      this.setState({
+        focus: 'left',
+      })
+    }
+    const rightClickHandler = () => {
+      console.log('right clicked!')
+      this.setState({
+        focus: 'right',
+      })
+    }
+
+    const { focus } = this.state
+    const leftClass = classNames({
+      "split-image": true,
+      "left-split": focus === 'center',
+      "left-split-left": focus === 'left',
+      "left-split-right": focus === 'right',
+    })
+    const rightClass = classNames({
+      "split-image": true,
+      "right-split": focus === 'center',
+      "right-split-left": focus === 'left',
+      "right-split-right": focus === 'right',
+    })
+    
+
     return (
       <div className="split-header">
-        <div className="split-image left-split" style={leftStyle}></div>
-        <div className="split-image right-split" style={rightStyle}></div>
+        <div 
+          className={leftClass} 
+          style={leftStyle}
+          onClick={leftClickHandler}
+        >
+        </div>
+        <div 
+          className={rightClass}
+          style={rightStyle}
+          onClick={rightClickHandler}
+        ></div>
       </div>
     )
   }
