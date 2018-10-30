8c1f334dd47a4da677e288775c70c718754caa16
Nathan Myers <nathanemyers@gmail.com>
2018-10-29 21:39:49 -0500

    final touches

--- src/App.js
+++ src/App.js
@@ -3,21 +3,34 @@ import './App.css';
 import SplitHeader from './components/SplitHeader'
 
 const zelda = "http://images.nintendolife.com/news/2017/03/feature_what_happens_when_you_play_breath_of_the_wild_as_a_multiplayer_game/attachment/0/original.jpg"
-const mgs = "https://www.technobuffalo.com/wp-content/uploads/2015/08/Yoji-Shinkawa-Metal-Gear-Solid-art-6.jpg"
-const dva = "https://gear.blizzard.com/media/catalog/product/o/w/ow-gold-dva-video-rough.jpg"
+const lttp = "https://cdn03.nintendo-europe.com/media/images/10_share_images/games_15/super_nintendo_5/H2x1_SNES_TheLegendOfZeldaALinkToThePast_image1600w.jpg"
 
+const ff7 = "http://the-top10list.com/wp-content/uploads/2013/12/best-final-fantasy-weapon-cloud.jpg"
+const ff8 = "http://img05.deviantart.net/6050/i/2015/006/8/5/final_fantasy_viii_title_screen_remake_by_darkwarriornyx-d8cxe8v.png"
 class App extends Component {
 
   render() {
     return (
       <div className="App">
         <SplitHeader 
+          leftImage={lttp}
+          leftTitle="A Link to The Past"
+          leftBody="The Legend of Zelda: A Link to the Past Review - Screenshot 1 of 2
+          To put it simply, The Legend of Zelda: A Link to the Past is one of the finest RPGs in existence."
           rightImage={zelda}
           rightTitle="Breath of the Wild"
-          rightBody="Can anyone make sense of the timeline??"
-          leftImage={dva}
-          leftTitle="Metal Gear Solid"
-          leftBody="Metal Gear Solid is basically an anime, and I'm okay with that. "
+          rightBody="The Legend of Zelda: Breath of the Wild is a masterclass in open-world design and a watershed game that reinvents a 30-year-old franchise."
+        />
+
+        <SplitHeader 
+          leftColor="white"
+          leftImage={ff7}
+          leftTitle="Final Fantasy VII"
+          leftBody="Final Fantasy VII, the latest output from RPG king Square, the end result justifies the delays without question. FF7 is, without a doubt, the new benchmark for console titles."
+          rightColor="white"
+          rightImage={ff8}
+          rightTitle="Final Fantasy VIII"
+          rightBody="Between the story, the beautiful graphics, music score, and the insanely enthralling card game mini-game, Final Fantasy VIII does the series proud."
         />
       </div>
     );
--- src/components/SplitHeader.js
+++ src/components/SplitHeader.js
@@ -33,9 +33,11 @@ class SplitHeader extends Component {
 
   render() {
     const { 
+      rightColor,
       rightImage, 
       rightTitle, 
       rightBody, 
+      leftColor,
       leftImage,
       leftTitle, 
       leftBody, 
@@ -43,9 +45,11 @@ class SplitHeader extends Component {
 
     const leftStyle = {
       backgroundImage: `url(${leftImage})`,
+      color: leftColor,
     }
     const rightStyle = {
       backgroundImage: `url(${rightImage})`,
+      color: rightColor,
     }
 
     const { focus, fullScreen } = this.state
@@ -77,46 +81,53 @@ class SplitHeader extends Component {
     })
 
     return (
-      <div className="split-header-container" onMouseLeave={() => this.setFocus('center')}>
+      <div 
+        className="split-header-container" 
+        onMouseLeave={() => {if (!fullScreen) { this.setFocus('center') }}}
+      >
         <div 
           className={leftClass} 
           style={leftStyle}
           onMouseEnter={() => this.setFocus('left')}
           onClick={this.openFullScreen}
         >
-          <button 
-            className={leftButton}
-            onClick={this.closeFullScreen}
-          >
-            Close
-          </button>
 
+        <button className={leftButton} onClick={this.closeFullScreen}>
+          Close
+        </button>
+
+        <div className="content">
           <h2>{leftTitle}</h2>
-          <p>{leftBody}</p>
-        </div>
-
-        <div 
-          className={rightClass}
-          style={rightStyle}
-          onMouseEnter={() => this.setFocus('right')}
-          onClick={this.openFullScreen}
-        >
-          <button 
-            className={rightButton}
-            onClick={this.closeFullScreen}
-          >
-            Close
-          </button>
-
-          <h2>{rightTitle}</h2>
-          <p>{rightBody}</p>
+          <p><strong>{leftBody}</strong></p>
         </div>
       </div>
+
+      <div 
+        className={rightClass}
+        style={rightStyle}
+        onMouseEnter={() => this.setFocus('right')}
+        onClick={this.openFullScreen}
+      >
+
+      <button className={rightButton} onClick={this.closeFullScreen}>
+        Close
+      </button>
+
+      <div className="content">
+
+        <h2>{rightTitle}</h2>
+        <p><strong>{rightBody}</strong></p>
+      </div>
+    </div>
+  </div>
     )
   }
 }
 
 SplitHeader.propTypes = {
+  rightColor: PropTypes.string,
+  leftColor: PropTypes.string,
+
   rightImage: PropTypes.string.isRequired,
   leftImage: PropTypes.string.isRequired,
 
